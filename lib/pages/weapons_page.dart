04965b0424cb0aa6715c9109c587cafc7de16e20
Ng Shu Kai Alden <aldenngsk@gmail.com>
2025-07-23 22:34:59 +0800

    adjusted appbar

--- lib/pages/weapons_page.dart
+++ lib/pages/weapons_page.dart
@@ -2,4 +2,16 @@ import 'package:flutter/material.dart';
 
 class WeaponsPage extends StatelessWidget {
   const WeaponsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Weapons'),
+      ),
+      body: const Center(
+        child: Text('This is the Weapons Page'),
+      ),
+    );
+  }
 } 
