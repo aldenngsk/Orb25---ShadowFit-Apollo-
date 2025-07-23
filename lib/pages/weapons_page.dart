e1fded10618b3283a73b30a43dccc35b349c3ed6
Ng Shu Kai Alden <aldenngsk@gmail.com>
2025-07-23 22:40:26 +0800

    add stats to weapons page

--- lib/pages/weapons_page.dart
+++ lib/pages/weapons_page.dart
@@ -1,24 +1,38 @@
 import 'package:flutter/material.dart';
 
+class Weapon {
+  final String name;
+  final int attack;
+  final int magic;
+  final String rarity;
+
+  const Weapon({
+    required this.name,
+    required this.attack,
+    required this.magic,
+    required this.rarity,
+  });
+}
+
 class WeaponsPage extends StatelessWidget {
   const WeaponsPage({Key? key}) : super(key: key);
 
-  final List<String> weapons = const [
-    'Demon King’s Dagger',
-    'Kamish’s Wrath',
-    'Baruka’s Dagger',
-    'Knight Killer',
-    'Demon Monarch’s Longsword',
-    'Demon Monarch’s Shortsword',
-    'Demon Monarch’s Daggers',
-    'Demon Monarch’s Armor',
-    'Demon Monarch’s Ring',
-    'Demon Monarch’s Necklace',
-    'Demon Monarch’s Earrings',
-    'Demon Monarch’s Belt',
-    'Demon Monarch’s Boots',
-    'Demon Monarch’s Gloves',
-    'Demon Monarch’s Cloak',
+  final List<Weapon> weapons = const [
+    Weapon(name: 'Demon King’s Dagger', attack: 120, magic: 80, rarity: 'Legendary'),
+    Weapon(name: 'Kamish’s Wrath', attack: 200, magic: 150, rarity: 'Mythic'),
+    Weapon(name: 'Baruka’s Dagger', attack: 90, magic: 60, rarity: 'Epic'),
+    Weapon(name: 'Knight Killer', attack: 100, magic: 40, rarity: 'Rare'),
+    Weapon(name: 'Demon Monarch’s Longsword', attack: 180, magic: 120, rarity: 'Legendary'),
+    Weapon(name: 'Demon Monarch’s Shortsword', attack: 160, magic: 110, rarity: 'Legendary'),
+    Weapon(name: 'Demon Monarch’s Daggers', attack: 140, magic: 100, rarity: 'Legendary'),
+    Weapon(name: 'Demon Monarch’s Armor', attack: 0, magic: 200, rarity: 'Legendary'),
+    Weapon(name: 'Demon Monarch’s Ring', attack: 20, magic: 180, rarity: 'Legendary'),
+    Weapon(name: 'Demon Monarch’s Necklace', attack: 15, magic: 170, rarity: 'Legendary'),
+    Weapon(name: 'Demon Monarch’s Earrings', attack: 10, magic: 160, rarity: 'Legendary'),
+    Weapon(name: 'Demon Monarch’s Belt', attack: 5, magic: 150, rarity: 'Legendary'),
+    Weapon(name: 'Demon Monarch’s Boots', attack: 0, magic: 140, rarity: 'Legendary'),
+    Weapon(name: 'Demon Monarch’s Gloves', attack: 25, magic: 130, rarity: 'Legendary'),
+    Weapon(name: 'Demon Monarch’s Cloak', attack: 0, magic: 120, rarity: 'Legendary'),
   ];
 
   @override
@@ -30,9 +44,11 @@ class WeaponsPage extends StatelessWidget {
       body: ListView.builder(
         itemCount: weapons.length,
         itemBuilder: (context, index) {
+          final weapon = weapons[index];
           return ListTile(
             leading: const Icon(Icons.security),
-            title: Text(weapons[index]),
+            title: Text(weapon.name),
+            subtitle: Text('ATK: ${weapon.attack}  |  MAG: ${weapon.magic}  |  Rarity: ${weapon.rarity}'),
           );
         },
       ),
