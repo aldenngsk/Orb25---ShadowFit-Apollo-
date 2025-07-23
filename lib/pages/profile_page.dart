913d282debd4557c576b6b2fbe1c2af45233a2e8
Ng Shu Kai Alden <aldenngsk@gmail.com>
2025-07-23 21:49:57 +0800

    adjusted appbar

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -5,6 +5,10 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile'),
+      )
+    );
   }
 } 
